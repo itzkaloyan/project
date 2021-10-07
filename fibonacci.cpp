7063309f4aa50ef3728b3b0cdcaa46a69bb4606f
itzkaloyan <kaloyanmi98@gmail.com>
2021-10-07 14:52:24 +0300

    fibonacci 2

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-int n1, n2, n3, elements,i;
-cout<<"Number of elements";
-cin>>elements;
-for(i=0;i<elements;i++)
-{
-n3=n1+n2;
-cout<<n3;
-n1=n2;
-n2=n3;
-}
-return 0;
+
+    int n1, n2, n3, elements,i;
+    cout<<"Number of elements";
+    cin>>elements;
+    for(i=0;i<elements;i++)
+    {
+        n3=n1+n2;
+        cout<<n3;
+        n1=n2;
+        n2=n3;
+    }
+    return 0;
 }
