f70f5ca87d401fb9f2090372459fb743c27e57ea
itzkaloyan <kaloyanmi98@gmail.com>
2021-10-08 12:24:45 +0300

    Reading file and writing new file

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,19 +1,32 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int num;
-    FILE *fptr;
-    fopen("test.txt","w");
+#include <stdio.h>
+#include <stdlib.h>
+#define DATA_SIZE 1000
+int main()
+{
+    char data[DATA_SIZE];
+    FILE * fptr;
+    fptr = fopen("test.txt", "r");
+
     if(fptr == NULL)
-   {
-      printf("Error");   
-      exit(1);             
-   }
-   printf("Enter num: ");
-   scanf("%d",&num);
-
-   fprintf(fptr,"%d",num);
-   fclose(fptr);
-
+    {
+        printf("Unable to create file.\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Enter contents to store in file:\n");
+    fgets(data, DATA_SIZE, fptr);
+    
+    fclose(fptr);
+    printf("File created and saved successfully\n");
+    
+    FILE * fptr2;
+    fptr2 = fopen("test2.txt", "w");
+    
+    if(fptr2 == NULL)
+    {
+        printf("Unable to create file.\n");
+        exit(EXIT_FAILURE);
+    }
+    fputs(data, fptr2);
+    fclose(fptr2);
     return 0;
 }
