bfbc227eb7c27bc07c2681da83b0bc9f91838748
itzkaloyan <91739526+itzkaloyan@users.noreply.github.com>
2021-11-04 10:25:02 +0200

    vulcano pattern

--- task.cpp
+++ task.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 int main(){
     int rows = 1;
-    int num = 1;
     printf("Enter number of rows:\n");
     scanf("%d",&rows);
-    for(int i=0;i<rows;i++){
-        for(int space = 1;space <= rows-i;space++)
-            printf("  ");
-            for(int j = 0;j <= i;j++){
-                if (j == 0 || i == 0){
-                    num = 1;
-                }
-                else {
-                    num = num*(i-j+1)/j;
-                }
-                printf("%3d ",num);
+    for(int i=0;i<=rows;i++){
+            for(int space = rows-1;space >= i;space--){
+                printf(" ");
             }
+            for(int j=1; j<=i; j++){
+            printf("*");
+            }
+            printf(" ");
+            for(int j=1;j<=i;j++){
+                printf("*");
+            }
+
             printf("\n");
     }
     return 0;
