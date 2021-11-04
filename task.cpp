2dcbf5f227259000f10235f479117f9796f41139
itzkaloyan <91739526+itzkaloyan@users.noreply.github.com>
2021-11-04 17:53:06 +0200

    trapeze(in progress)

--- task.cpp
+++ task.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 using namespace std;
+void firstline(int rows);
+void lastline(int rows);
+void midlines(int rows);
 int main(){
-    int rows = 0;
-    int space =0;
-    int line = 0;
-    printf("Enter number of rows:\n");
-    scanf("%d",&rows);
-    for(int i=0;i<rows;i++){
-        for (line = i; line <= rows; line++){
-            printf(" ");
-        }
-        printf("/");
-        for (space=0;space<i+i;space++){
-            printf(" ");
-        }
-        if(line > space){
-            printf("_");
-        }
-        printf("\\");
-        printf("\n");
-        
-    }  
+    int rows;
+    cout << "Enter length:\n";
+    cin >> rows;
+    firstline(rows);
+    midlines(rows);
+    lastline(rows);
     return 0;
+}
+void firstline(int rows){
+    for(int i=0;i<rows-1;i++){
+        cout << " ";
+    }
+    for(int i=0;i<rows;i++){
+        cout << "* ";
+    }
+    cout << endl;
+}
+void midlines(int rows){
+    for(int i=1; i<rows-1; i++){
+        for(int j=0;j<i+1;j++){
+            cout << " ";
+        }
+        cout << "*";
+        cout << endl;
+    }
+}
+void lastline(int rows){
+    for(int i=0;i<rows+rows-1;i++){
+        cout << "* ";
+    }
 }
