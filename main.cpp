873579470eb7447e1c1dd65999c75ab5becf338e
itzkaloyan <91739526+itzkaloyan@users.noreply.github.com>
2021-11-12 11:25:12 +0200

    maze game (via file) DONE

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void box(int n, int m, char map[256][256], int posX, int posY, FILE * fptr);
-void print(char map[256][256]);
+void draw(char map[256][256], int h);
 
-int main()
-{
+int main() {
     system("stty cooked");
-    int n = 22;
-    int m = 10;
-    char map[256][256];
     FILE * fptr;
     fptr = fopen("test.txt", "r");
     if(fptr == NULL)
@@ -17,53 +12,56 @@ int main()
         printf("Unable to open file.\n");
         exit(EXIT_FAILURE);
     }
+    int h = 0;
+    int w = 0;
+    char map[256][256];
     int posX;
     int posY;
     fscanf(fptr,"%d %d\n", &posX, &posY);
-    if(posX>=n||posY>=m){
+    for (int i = 0; i < 200; i++)
+    {
+        char * c = fgets(map[i],200,fptr);
+        if(c == 0) {
+            h = i;
+            break;
+        }
+    }
+    for (short i = 0; i < 256; i++)
+    {
+        if (map[0][i] == '#') w++;
+    }
+    printf("Size: h: %d w: %d \n", h, w);
+    if(posX>=w || posY>=h || posX<=0 || posY<=0) {
         cout << "Wrong info\n";
-        return 0;
+        return EXIT_FAILURE;
     }
     char ch;
-    system("stty raw");
-    
-    box(n, m, map, posX, posY, fptr);
-    
-    while ((ch = getchar()) != 'e') {
-        print(map);
-        if ((ch == 'w' || ch == 'W') && map[posX][posY-1] != '#'){
-            posY -= 1;
-        }
-        if ((ch == 's' || ch == 'S') && map[posX][posY+1] != '#'){
-            posY += 1;
-        }
-        if ((ch == 'a' || ch == 'A') && map[posX-1][posY] != '#'){
-            posX -= 1;
-        }
-        if ((ch == 'd' || ch == 'D') && map[posX+1][posY] != '#'){
-            posX += 1;
-        }
+    while ((ch = getchar()) != '\e')
+    {
+        system("stty raw");
         system("clear");
+        map[posY][posX] = ' ';
+        switch (ch) {
+            case 'W': case 'w':
+            if (map[posY-1][posX] != '#') posY--;
+            break;
+            case 'S': case 's':
+            if (map[posY+1][posX] != '#') posY++;
+            break;
+            case 'A': case 'a':
+            if (map[posY][posX-1] != '#') posX--;
+            break;
+            case 'D': case 'd':
+            if (map[posY][posX+1] != '#') posX++;
+            break;
+        }
+        map[posY][posX] = '@';
+        draw(map,h);
     }
     system("stty cooked");
     return 0;
 }
-void box(int n, int m, char map[256][256], int posX, int posY, FILE * fptr)
-{
-    printf("\n\r");
-    int i = 0;
-    map[posX][posY+1]= '@';
-    for (i; i < 200; i++)
-    {
-        char * c = fgets(map[i],200,fptr);
-        if(c == 0){
-            break;
-        }
-    }
+void draw(char map[256][256], int h) {
+    for (int i = 0; i < h; i++)
+       printf("%s \r", map[i]);
 }
-void print(char map[256][256]){
-    for(int i; i < 200; i++){
-    printf("%d %s\r", i, map[i]);
-    }
-}
-
