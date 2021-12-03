e2f3c3165b967df736df278b31fe0cf37ee6b65e
itzkaloyan <91739526+itzkaloyan@users.noreply.github.com>
2021-12-03 16:12:23 +0200

    Add files via upload

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 int main()
 {
+    printf("%d %d\n", sizeof(long long),sizeof(float));
+    int n=0;
+    int m=0;
+    int num=0;
     FILE * fptr;
     fptr = fopen("test.txt", "r");
     if(fptr == NULL)
@@ -9,25 +13,18 @@ int main()
         printf("Unable to create file.\n");
         exit(EXIT_FAILURE);
     }
-    int num;
-    fscanf(fptr,"%d", &num);
-    printf("Number of lines:%d\n", num);
-    
-    int sum=0;
-    int Fnum=0;
-    int Snum=0;
-    for(int i = 0; i < num; i++)
+    fscanf(fptr,"%d", &n);
+    for(int i = 0; i < n; i++)
     {
-        int res = fscanf(fptr,"%d %d\n",&Fnum,& Snum);
-        if (res != 2) {
-            printf("Invalid input line %d\n", i);
-            continue;
+        int sum=0;
+        fscanf(fptr,"%d",&m);
+        for (int l=0;l<m;l++) {
+            fscanf(fptr, "%d",&num);
+            sum +=num;
         }
-        sum=Fnum+Snum;
-        printf("Sum of numbers is: %i\n", sum); 
-
+        float avg=sum/float(m);
+        printf("Avg of the line is:%d %f %d\n",avg, sum, m);
     }
-    fclose(fptr);
-    printf("File read succesfully\n");   
+    fclose(fptr);  
     return 0;
 }
