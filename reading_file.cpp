e4c6056a196b42762a95781e9d24968581df3ab4
itzkaloyan <kaloyanmi98@gmail.com>
2021-12-16 17:02:46 +0200

    radar part 1

--- reading_file.cpp
+++ reading_file.cpp
@@ -1,80 +1,72 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-void box(char map[16][16], int r){
-    for (int i = 0; i <= r; i++){
-        for(int j = 0;j <=r; j++){
-            char map[i][j]=' ';
+void drawline(char map[128][128],int x1, int y1,int x2,int y2)  {  
+    int lastX = x2;
+    int lastY = y2;
+    float err;
+    int x = x1;
+    int y = y1;
+    float tang = float((y2-y1)/(x2-x1));
+    if(x1<=x2){
+        while(x != x2){
+            err += tang;
+            cout << "(" << x << "," << y << ")\n";
+            map[y][x]='#';
+            x++;
+            if (err > 0){
+                y++;
+            }
+        }
+    }
+    if(x1>=x2){
+        while(x != x2){
+            map[y][x]='#';
+            x--;
+        }
+    }
+    if(y1<=y2){
+        while(y != y2){
+            map[y][x]='#';
+            y++;
+        }
+    }
+    if(y1>=y2){
+        while(y != y2){
+            map[y][x]='#';
+            y--;
         }
     }
 }
 
-void drawline(char map[16][16],int x1, int y1,int x2,int y2)  
-{  
-    int dx, dy, p, x, y; 
-    dx=x2-x1;  
-    dy=y2-y1;  
-    x=x1;  
-    y=y1;  
-    p=2*dy-dx;
-    float x = 1;
-    float y = 0;
-    int angle = 90;
-    while(true){
-        if(){
-            return 0;
-        }
-        if(){
-            angle += 5;
-        }      
-        else if(){
-            angle -= 5;
-        }
-        if(angle > 360){
-            angle -= 360;
-        }
-        if(angle < 0){
-            angle += 360;
-        }
-        if(angle >= 45 && angle <= 135){
-            y = -1 + 1/45.0*(angle-45);
-        }
-        else if(angle >= 225 && angle <= 315){
-            y = 1 - 1/45.0*(angle-225);
-        }
-        if(angle >= 135 && angle <= 225){
-            x = 1 - 1/45.0 * (angle-135);
-        }
-        else if(angle >= 315){
-            x = -1 + 1/45.0 * (angle-315);
-        }
-        else if(angle <= 45){
-            x = 1/45.0 * angle;
-        }
-        if(angle == 360){
-            angle = 0;
+void box(char map[128][128], int size, int x1, int y1,int x2,int y2){
+    for (int i = 0; i < size; i++){
+        for(int j = 0;j < size; j++){
+            map[i][j]= '.';
         }
     }
+    drawline(map,x1,y1,x2,y2);
+    for (int i = 0; i < size; i++){
+        for(int j = 0;j < size; j++){
+            printf("%c ",map[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
 
 int main(){
-    box(map,r);
-    int r;
+    int radius = 3;
+    int size = radius*2+1;
     int x2;
+    cin>>x2;
     int y2;
-    cout<<"enter ending point";
-    cin>>x2>>y2;
-    cout<<"enter radius:";
-    cin>>r;
-    int x1 = r;
-    int y1 = r;
-    char map[16][16];
-    for (int i = 0; i <= r*r; i++){
-        for(int j =0; j <= r*r; j++){
-            map[i][j]=' ';
-        }
-    }
-    map[x1][y1] = '#';
-    drawline(map,x1,y1,x2,y2);
-    box(map,r);
+    cin>>y2;
+    int x1 = radius;
+    int y1 = radius;
+    char map[128][128];
+    box(map, size, x1, y1, x2, y2);
+    
 }
 
