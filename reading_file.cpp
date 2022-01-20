0cdbe0b75742332996bdee5314677abdbc38bb07
itzkaloyan <kaloyanmi98@gmail.com>
2022-01-20 17:40:20 +0200

    radar progress

--- reading_file.cpp
+++ reading_file.cpp
@@ -9,20 +9,20 @@ using namespace std;
 void setMap(char map[128][128], int x, int y, char c) {
     map[y][x]=c;
 }
-void spin(int* x, int* y, int rad) {
+void spin(int* x, int* y, int size) {
     int x1 = *x;
     int y1 = *y;
-    cout<<x1<<":"<<y1<<endl;
-    if (x1 >= 0 && x1 < rad && y1 == 0) {
+    //cout<<x1<<":"<<y1<<endl;
+    if (x1 >= 0 && x1 < size && y1 == 0) {
         x1++;
     }
-    else if (x1 >= rad && y1 >= 0 && y1 < rad) {
+    else if (x1 >= size && y1 >= 0 && y1 < size) {
         y1++;
     }
-    else if (x1 > 0 && x1 <= rad && y1 == rad) {
+    else if (x1 > 0 && x1 <= size && y1 == size) {
         x1--;
     }
-    else if (x1 == 0 && y1 > 0 && y1 <= rad) {
+    else if (x1 == 0 && y1 > 0 && y1 <= size) {
         y1--;
     }
     cout<<x1<<":"<<y1<<endl;
@@ -31,117 +31,103 @@ void spin(int* x, int* y, int rad) {
 }
 void drawline(char map[128][128],int x1, int y1,int x2,int y2,int size, int radius)  {  
     assert(map != nullptr);
-    float dx = x2 - x1;
-    float dy = y2 - y1;
+    float dx = abs(x2 - x1);
+    float dy = abs(y2 - y1);
     float err = 0;
     int x = x1;
     int y = y1;
     float limit;
     if(x2==size-1){
+        assert(dx != 0);
         float tang = float(dy)/dx;
         while(x != x2+1){
             limit = sqrt(((y1-y)*(y1-y))+((x1-x)*(x1-x)));
             assert(x >= 0 && y >= 0 && x <= size + 1 && y <= size + 1);
-            err += tang;
+            err += tang; 
+            int step = (y>=y2) ? -1 : 1;
+            if(err >= 0.5){
+                err -=1;
+                y += step;
+            }
             if (limit < radius) {
                 setMap(map,x, y, '#');
             }
             x++;
-            if(y>=y2){
-                if(err <= 0.5){
-                    y--;
-                    err += 1;
-                }
-            }
-            if(y<y2){
-                if(err >= 0.5){
-                    y++;
-                    err -= 1;
-                }
-            }
+            
         }
     }
     else if(x2==0){
+        assert(dx != 0);
         float tang = float(dy/dx);
         while(x != x2-1){
             limit = sqrt(((y1-y)*(y1-y))+((x1-x)*(x1-x)));
             assert(x >= 0 && y >= 0 && x <= size + 1 && y <= size + 1);
+            int step = (y>=y2) ? -1 : 1;
+            if(err >= 0.5) {
+                err -=1;
+                y += step;
+            }
             err += tang;
             if (limit < radius) {
                 setMap(map,x, y, '#');
             }
             x--;
-            if(y>y2){
-                if(err >= 0.5){
-                    y--;
-                    err -= 1;
-                }
-            }
-            if(y<y2){
-                if(err <= 0.5){
-                    y++;
-                    err += 1;
-                }
-            }
+
         }
     }
     else if(y2==0&&x2!=0){
+        assert(dy != 0);
         float tang = float(dx/dy);
         while(y != y2-1){
             limit = sqrt(((y1-y)*(y1-y))+((x1-x)*(x1-x)));
-            assert(x >= 0 && y >= 0 && x <= size + 1 && y <= size + 1);
+            assert(x >= 0 && y >= 0 && x <= size + 1 && y <= size + 1);  
+            int step = (x>=x2) ? -1 : 1;
+            if(err >= 0.5){
+                err -=1;
+                x += step;
+            }
             err += tang;
             if (limit < radius) {
                 setMap(map,x, y, '#');
             }
             y--;
-            if(x>x2){
-                if(err >= 0.5){
-                    x++;
-                    err -= 1;
-                }
-            }
-            if(x<x2){
-                if(err <= 0.5){
-                    x--;
-                    err += 1;
-                }
-            }
         }
     }
     else if(y2==size-1){
+        assert(dy != 0);
         float tang = float(dx/dy);
         while(y != y2+1){
             limit = sqrt(((y1-y)*(y1-y))+((x1-x)*(x1-x)));
-            //assert(x >= 0 && y >= 0 && x <= size + 1 && y <= size + 1);
+            assert(x >= 0 && y >= 0 && x <= size + 1 && y <= size + 1);
+            int step = (x>=x2) ? -1 : 1;
+            if(err >= 0.5){
+                err -= 1;
+                x += step;
+            }
             err += tang;
             if (limit < radius) {
                 setMap(map,x, y, '#');
             }
             y++;
-            if(x>x2){
-                if(err <= 0.5){
-                    x--;
-                    err += 1;
-                }
-            }
-            if(x<x2){
-                if(err >= 0.5){
-                    x++;
-                    err -= 1;
-                }
-            }
         }
     }
 }
 
-void box(char map[128][128], int size, int x1, int y1,int x2,int y2, int radius){
+void initMap(char map[128][128], int size) {
     for (int i = 0; i < size; i++){
         for(int j = 0;j < size; j++){
             setMap(map,i, j, '.');
         }
     }
+}
+void box(char map[128][128], int size, int x1, int y1,int x2,int y2, int radius){
+    //Initilize the grid with . everywhere
+    initMap(map, size);
+
+    // Draw the Radar line on the grid
     drawline(map,x1,y1,x2,y2,size,radius);
+
+    // Draw the final result on the screen
     for (int i = 0; i < size; i++){
         for(int j = 0;j < size; j++){
             printf("%c ",map[i][j]);
@@ -153,32 +139,39 @@ void box(char map[128][128], int size, int x1, int y1,int x2,int y2, int radius)
 
 int main(){
     int radius = 6;
-    int size = radius*2+1;
+    int size = (radius*2)+1;
     int x1 = radius;
     int y1 = radius;
     char map[128][128];
     int x2 = radius;  
     int y2 = 0;
     char key;
-    do{
-        system("clear");
-        spin(&x2, &y2, radius*2);
-        box(map, size, x1, y1, x2, y2, radius);
-        // system("stty raw");
-        // cin>>key;
-        // system("stty cooked");
-        // if (key=='e') {
-        //     return 0;
-        // }
-        // if (key=='d') {
+    
+    if (0) {
+        x2=0; y2=4;
+        box(map, size, x1, y1, x2, y2, 100);
+    } else {
+        do{
             
-        // }
-        // if(key=='a') {
+            spin(&x2, &y2, size -1);
+            box(map, size, x1, y1, x2, y2, 100);
+            // system("stty raw");
+            // cin>>key;
+            // system("stty cooked");
+            // if (key=='e') {
+            //     return 0;
+            // }
+            // if (key=='d') {
+                
+            // }
+            // if(key=='a') {
 
-        // }
-        usleep(1000*100);
-        
-    }while(true);
+            // }
+            usleep(1000*1000);
+            system("clear");
+            
+        }while(true);
+    }
 }
 
 
